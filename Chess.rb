c04db7f61035c9f9e798f541fdeb1b6b2e0b2960
Yani Maltsev <yani.maltsev@gmail.com>
2014-12-29 10:53:37 +0200

    Adding Chess player options choise
    The Game starts as command line, the player can chose to change that to
    2D as well as the chess rules.

--- Chess.rb
+++ Chess.rb
@@ -1,16 +1,33 @@
-
-
 module Chess
   class Board
+  private
+    attr_accessor :board, :rule_type, :render_type
+	
+	def init_game_type
+	  choise = 0
+	  until (1..3) === choise do
+	    puts "Chose Chess rules: 1. Standart 2. timed 3. ???"
+		choise = gets.to_i
+	  end
+	  @rule_type = choise
+	  choise = 0
+	  until (1..2) === choise do
+	    puts "Chose Chess Render type: 1. Command Line 2. 2D"
+		choise = gets.to_i
+	  end
+	  @render_type = choise
+	end
+	
+  public
     def initialize
       board = Hash.new(nil)
+	  init_game_type
     end
 
     def run
 	  
     end
 	
-  private
-    attr_accessor :board
+ 
   end
 end
