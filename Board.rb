3d58bb94d629a8b4055756b93ef33d960873bdc7
Yani Maltsev <yani.maltsev@gmail.com>
2014-12-31 16:15:28 +0200

    Implemented CL renderer, modified board
    Board now has reset function for easier reset form the renderers.
    
    CL renderer has the old functionality of the board's render. Board's
    render removed.

--- Board.rb
+++ Board.rb
@@ -4,26 +4,9 @@ require "./King.rb"
 require "./Knight.rb"
 module Chess
   class Board
+  
+  attr_accessor :board, :rule_type, :render_type, :selected , :possible_moves, :turnColor, :winner, :last_time, :turn_time, :turn_time_playerW, :turn_time_playerB
   private
-    attr_accessor :board, :rule_type, :render_type, :selected , :selector, :possible_moves, :turnColor, :winner, :last_time, :turn_time, :turn_time_playerW, :turn_time_playerB
-    
-    def init_game_type
-      choise = 0
-      until (1..3) === choise do
-        puts "Chose Chess rules: 1. Standart 2. Timed 3. ???"
-        choise = gets.strip.to_i
-      end
-      @rule_type = choise
-      
-      choise = 0
-      until (1..2) === choise do
-        puts "Chose Chess Render type: 1. Command Line 2. 2D"
-        choise = gets.strip.to_i
-      end
-      @render_type = choise
-    end
-    
-    
     def init_board
       File.open('BoardSave.txt', 'r') do |f1| 
         if f1.gets.split[0].to_i !=@rule_type 
@@ -31,33 +14,38 @@ module Chess
         else
           choise = "a"
           until choise == "y" || choise == "n" do
-              print "Load last game?(y/n):"
+            print "Load last game?(y/n):"
             choise = gets.strip
           end
           reset_board if choise == "n"
         end
-		@turnColor = f1.gets.split[0]
+      end
+	  
+	  load_boad
+    end
+    
+	def load_boad
+	  File.open('BoardSave.txt', 'r') do |f1| 
+        f1.gets
+        @turnColor = f1.gets.split[0]
         (1..8).each do |y|
           f1.gets.split.each_with_index{ |piece, x | @board[[x+1,y]] = (piece == "n")? nil:piece}
           board.each{| k,v| Pawn.add_starting_pawn(k) if v!= nil && v[1]=="P"}
         end
-		if(rule_type == 2 ) 
-		  @last_time=Time.now 
-		  @turn_time = 10  
-		end
-		
-		if(rule_type == 3 ) 
-		  @last_time=Time.now 
-		  @turn_time_playerW = 300
-		  @turn_time_playerB = 300
-		end
       end
-    end
-    
-    def init_2D
-    
-    end
-    
+	  
+	  
+	  if(rule_type == 2 ) 
+	    @last_time=Time.now 
+	    @turn_time = 10  
+	  end
+	  if(rule_type == 3 ) 
+	    @last_time=Time.now 
+	    @turn_time_playerW = 300
+	    @turn_time_playerB = 300
+	  end
+	end
+	
     def reset_board
       File.open('BoardSave.txt', 'w') do |f1|
         f1.puts "#{@rule_type}"
@@ -83,87 +71,6 @@ module Chess
       end
 	end
 	
-    def render_player_controls
-      puts "//----------------------------------------------------------"
-      puts "// to select piece/position input its coordinates, example:"
-      puts "// User input: a2"
-      puts "//To reset the game - User input: reset"
-      puts "//To exit the game - User input: exit"
-      puts "//----------------------------------------------------------"
-    end
-	
-    def render_board
-      system('cls')
-      render_player_controls
-      puts "  a  b  c  d  e  f  g  h"
-      puts board.values.map{ |x| x == nil ? x="  " : x }
-       .join("|").scan(/.{1,24}/).join("\n|")
-       .insert(0, "|").insert(-1, "|")
-       .split("\n").each_with_index.map{ |x,i| x = (i+1).to_s+x+(i+1).to_s}.join("\n")
-      puts "  a  b  c  d  e  f  g  h"
-      puts "Turn: #{ turnColor == "W"? "white" : "black"}"
-      puts "Selected piece:#{@selected == nil ? "none" : "[#{(selected[0]+96).chr}, #{selected[1]}]"}"
-      puts "possible move positions:#{@possible_moves == nil ? "none" : @possible_moves.collect{|x| [(x[0]+96).chr,x[1]] }}"
-    end
-    
-    def handle_user_input
-	  handle_time if( rule_type == 2 || rule_type == 3)
-      if(render_type == 1)
-	    render_board
-		render_time if (rule_type == 2 || rule_type == 3)
-		puts "Winner is: #{ winner == "W" ? "White" : "Black"}" if(winner !=nil)
-        print "User Input: "
-        input = gets
-        until input=="reset\n" || input=="exit\n" || (winner == nil && input.length-1==2 && (1..8) === input[0].ord-96 && (1..8) === input[1].to_i) do
-          print "Invalid input, please reenter input: "
-          input = gets
-        end
-        case input
-          when "reset\n" then initialize
-          when "exit\n" then return false
-          else
-            @selector = [input[0].ord-96,input[1].to_i]
-            handle_event unless rule_type == 2 && turn_time <0
-        end
-      end
-      true
-    end
-    
-	def handle_time
-	  time =  @last_time - Time.now
-	  @last_time = Time.now
-	  if (rule_type == 2)
-	    @turn_time += time
-		if turn_time < 0 
-		  @turn_time = 30
-		  @turnColor = @turnColor == "W"? "B": "W"
-		end
-	  end
-	  if (rule_type == 3)
-		if turnColor == "W"  
-		  @turn_time_playerW += time
-		  @winner = "B" if @turn_time_playerW < 0
-		else
-		  @turn_time_playerB += time
-		  @winner = "W" if @turn_time_playerB < 0
-		end
-	  end
-	end
-	
-	def render_time
-	  if render_type == 1
-	    print "Time left = "
-	    if rule_type == 2
-		  puts @turn_time.to_s 
-		else 
-		  if turnColor == "W"
-		    puts @turn_time_playerW.to_s
-		  else
-		    puts @turn_time_playerB.to_s
-		  end
-		end
-	  end
-	end
     def get_piece_moves(position, color)
       moves = []
       case board[position][1]
@@ -179,32 +86,7 @@ module Chess
     end
      
     
-    def handle_event
-      if(@board[@selector] !=nil && board[@selector][0]==@turnColor)
-        @selected = @selector
-        @possible_moves = get_piece_moves(selector, turnColor)
-      elsif @selected !=nil &&  (@possible_moves.include? @selector)
-	    temp = board[@selector]
-        board[@selector] = board[@selected]
-        board[@selected] = nil
-		if ( chess_check( turnColor, "K"))
-		  board[@selected] = board[@selector]
-		  board[@selector] = temp
-		else
-		  modify_pawn(selector) if(board[selector][1]== "P" || "p") 
-		  @selected = nil
-          @possible_moves = []
-		  @turnColor= @turnColor == "W" ? "B": "W"
-		  if(chess_check( turnColor, "K") && chessmate_check(turnColor, "K", selector)) 
-		    @winner = @turnColor == "W" ? "B": "W"
-		  end
-		end
-      else
-        @selected = nil
-        @possible_moves = []
-      end
-	  save_board
-    end
+    
 	
 	def modify_pawn(pos)
 	  if(turnColor == "W" && pos[1] ==1) ||  (turnColor == "B" && pos[1] ==8) 
@@ -250,28 +132,87 @@ module Chess
 	  result
 	end
 	
+	def handle_time
+	  time =  @last_time - Time.now
+	  @last_time = Time.now
+	  if (rule_type == 2)
+	    @turn_time += time
+		if turn_time < 0 
+		  @turn_time = 30
+		  @turnColor = @turnColor == "W"? "B": "W"
+		end
+	  end
+	  if (rule_type == 3)
+		if turnColor == "W"  
+		  @turn_time_playerW += time
+		  @winner = "B" if @turn_time_playerW < 0
+		else
+		  @turn_time_playerB += time
+		  @winner = "W" if @turn_time_playerB < 0
+		end
+	  end
+	end
+	
   public
-    def initialize
+    def initialize(type)
+	  @rule_type = type
       @board = Hash.new(nil)
-      init_game_type
+      #init_game_type
       init_board
-      init_2D if @render_type == 2
+      #init_2D if @render_type == 2
       selected = nil
       possible_moves = nil
 	  winner = nil
     end
-
-    def run
-      while true do
-        unless handle_user_input 
-          break 
+    def update
+	   handle_time if( rule_type == 2 || rule_type == 3)
+	end
+	
+	def reset
+	  reset_board
+	  load_boad
+	end
+	
+	def handle_event(selector)
+	  unless @rule_type == 2 && @turn_time <0 
+        if(@board[selector] !=nil && board[selector][0]==@turnColor)
+          @selected = selector
+          @possible_moves = get_piece_moves(selector, turnColor)
+        elsif @selected !=nil &&  (@possible_moves.include? selector)
+	      temp = board[selector]
+          board[selector] = board[@selected]
+          board[@selected] = nil
+		  if ( chess_check( turnColor, "K"))
+		    board[@selected] = board[selector]
+		    board[selector] = temp
+		  else
+		    modify_pawn(selector) if(board[selector][1]== "P" || "p") 
+		    @selected = nil
+            @possible_moves = []
+		    @turnColor= @turnColor == "W" ? "B": "W"
+		    if(chess_check( turnColor, "K") && chessmate_check(turnColor, "K", selector)) 
+		      @winner = @turnColor == "W" ? "B": "W"
+		    end
+		  end
+        else
+          @selected = nil
+          @possible_moves = []
         end
-      end
+	    save_board
+	  end
     end
+	
+    #def run
+    #  while true do
+    #    unless handle_user_input 
+    #      break 
+    #    end
+    #  end
+    #end
     
  
   end
 end
 
-chess = Chess::Board.new
-chess.run
+#chess = Chess::Board.new
+#chess.run
--- Game.rb
+++ Game.rb
@@ -1,15 +1,20 @@
+require "./Board.rb"
 module Chess
   class Game
+    
     def initialize
       init_game_type
-      init_board
-      init_2D if @render_type == 2
+	  if @render_type == 2
+        @Renderer = Renderer2D.new(@rule_type)
+      else
+        @Renderer = RendererCL.new(@rule_type)
+      end		
     end
 	
 	def init_game_type
       choise = 0
       until (1..3) === choise do
-        puts "Chose Chess rules: 1. Standart 2. Timed 3. ???"
+        puts "Chose Chess rules: 1. Standart 2. Timed rounds 3. Timed match"
         choise = gets.strip.to_i
       end
       @rule_type = choise
@@ -22,13 +27,93 @@ module Chess
       @render_type = choise
     end
 	
+	def run
+	  @Renderer.show
+	end
+	
+	
+	
   end
   
-  class 2Drenderer
+  class Renderer2D
     
-	def initialize
-	
+	def initialize(type)
+	  @board = Board.new(type)
 	end
   end
+  
+  class RendererCL    
+	def initialize(type)
+	  @board = Board.new(type)
+	end
 
+	def render_player_controls
+      puts "//----------------------------------------------------------"
+      puts "// to select piece/position input its coordinates, example:"
+      puts "// User input: a2"
+      puts "//To reset the game - User input: reset"
+      puts "//To exit the game - User input: exit"
+      puts "//----------------------------------------------------------"
+    end
+	
+	def render_time  
+      print "Time left = "
+	  if @board.rule_type == 2
+	    puts @board.turn_time.to_s 
+	  else 
+	    if @board.turnColor == "W"
+	      puts @board.turn_time_playerW.to_s
+	    else
+	      puts @board.turn_time_playerB.to_s
+	    end
+	  end
+	end
+	
+	def render_board
+      system('cls')
+      render_player_controls
+      puts "  a  b  c  d  e  f  g  h"
+      puts @board.board.values.map{ |x| x == nil ? x="  " : x }
+       .join("|").scan(/.{1,24}/).join("\n|")
+       .insert(0, "|").insert(-1, "|")
+       .split("\n").each_with_index.map{ |x,i| x = (i+1).to_s+x+(i+1).to_s}.join("\n")
+      puts "  a  b  c  d  e  f  g  h"
+      puts "Turn: #{ @board.turnColor == "W"? "white" : "black"}"
+      puts "Selected piece:#{@board.selected == nil ? "none" : "[#{(@board.selected[0]+96).chr}, #{@board.selected[1]}]"}"
+      puts "possible move positions:#{@board.possible_moves == nil ? "none" : @board.possible_moves.collect{|x| [(x[0]+96).chr,x[1]] }}"
+    end
+	
+	def handle_user_input
+	    @board.update
+	    render_board
+		render_time if (@board.rule_type == 2 || @board.rule_type == 3)
+		puts "Winner is: #{ @board.winner == "W" ? "White" : "Black"}" if(@board.winner !=nil)
+        print "User Input: "
+        input = gets
+        until input=="reset\n" || input=="exit\n" || (@board.winner == nil && input.length-1==2 && (1..8) === input[0].ord-96 && (1..8) === input[1].to_i) do
+          print "Invalid input, please re-enter input: "
+          input = gets
+        end
+        case input
+          when "reset\n" then @board.reset
+          when "exit\n" then return false
+          else
+            @board.handle_event([input[0].ord-96,input[1].to_i])
+        end
+      
+      true
+    end
+	
+	def show
+	  while true do
+        unless handle_user_input 
+		  break
+		end
+	  end
+	end
+	
+  end
 end
+
+game =  Chess::Game.new
+game.run
